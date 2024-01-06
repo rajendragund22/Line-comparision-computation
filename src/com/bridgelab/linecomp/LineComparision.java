746a11e87bff834be67dbec161ad4b66598b4dde
Rajendra Gund <rajendragund22@gmail.com>
2024-01-06 10:01:30 +0530

    [Rajendra] Update : Updated the code for length calculation

--- src/com/bridgelab/linecomp/LineComparision.java
+++ src/com/bridgelab/linecomp/LineComparision.java
@@ -1,9 +1,23 @@
 package com.bridgelab.linecomp;
 
+import java.util.Scanner;
+
 public class LineComparision {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter x co-ordinate of first point: ");
+		int x1 = sc.nextInt();
+		System.out.println("Enter y co-ordinate of first point: ");
+		int y1 = sc.nextInt();
+		System.out.println("Enter x co-ordinate of second point: ");
+		int x2 = sc.nextInt();
+		System.out.println("Enter y co-ordinate of second point: ");
+		int y2 = sc.nextInt();
+		sc.close();
+		double lengthOfLine = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+		System.out.println("Length of Line is : " + lengthOfLine);
 
 	}
 
