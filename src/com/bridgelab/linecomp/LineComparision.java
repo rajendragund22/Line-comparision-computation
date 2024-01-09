a1c35081aabba961ce4a85bcfb247c40e4384461
Rajendra Gund <rajendragund22@gmail.com>
2024-01-09 22:30:50 +0530

    [Rajendra] Update : Updated the code using equal method

--- src/com/bridgelab/linecomp/LineComparision.java
+++ src/com/bridgelab/linecomp/LineComparision.java
@@ -15,10 +15,31 @@ public class LineComparision {
 		int x2 = sc.nextInt();
 		System.out.println("Enter y co-ordinate of second point: ");
 		int y2 = sc.nextInt();
-		sc.close();
-		double lengthOfLine = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-		System.out.println("Length of Line is : " + lengthOfLine);
 
+		System.out.println("Enter p co-ordinate of first point: ");
+		int p1 = sc.nextInt();
+		System.out.println("Enter q co-ordinate of first point: ");
+		int q1 = sc.nextInt();
+		System.out.println("Enter p co-ordinate of second point: ");
+		int p2 = sc.nextInt();
+		System.out.println("Enter q co-ordinate of second point: ");
+		int q2 = sc.nextInt();
+		sc.close();
+		
+		double lengthOfLine1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		double lengthOfLine2 = Math.sqrt(Math.pow((p2 - p1), 2) + Math.pow((q2 - q1), 2));
+		System.out.println("Length of Line is : " + lengthOfLine1);
+		System.out.println("Length of Line is : " + lengthOfLine2);
+		
+		Double line1=  Double.valueOf(lengthOfLine1);
+		Double line2=  Double.valueOf(lengthOfLine2);
+		
+		boolean check=line1.equals(line2 );
+		
+		if(check)
+			System.out.println("Both lines are equal.");
+		else
+			System.out.println("Both lines are not equal.");
 	}
 
 }
