469fd9bc5c9a07e701943d072d5aca78ba4a2d0e
Rajendra Gund <rajendragund22@gmail.com>
2024-01-10 11:07:57 +0530

    [Rajendra] Update : Updated the code with compared both line using compareTo method

--- src/com/bridgelab/linecomp/LineComparision.java
+++ src/com/bridgelab/linecomp/LineComparision.java
@@ -25,18 +25,22 @@ public class LineComparision {
 		System.out.println("Enter q co-ordinate of second point: ");
 		int q2 = sc.nextInt();
 		sc.close();
-		
+
 		double lengthOfLine1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 		double lengthOfLine2 = Math.sqrt(Math.pow((p2 - p1), 2) + Math.pow((q2 - q1), 2));
 		System.out.println("Length of Line is : " + lengthOfLine1);
 		System.out.println("Length of Line is : " + lengthOfLine2);
-		
-		Double line1=  Double.valueOf(lengthOfLine1);
-		Double line2=  Double.valueOf(lengthOfLine2);
-		
+
+		Double line1 = Double.valueOf(lengthOfLine1);
+		Double line2 = Double.valueOf(lengthOfLine2);
 		int comp = line1.compareTo(line2);
 		System.out.println("Length of Line is : " + comp);
-		
+		if (comp > 0)
+			System.out.println("Length of first end points is greater than length of second end points");
+		else if (comp < 0)
+			System.out.println("Length of first end points is less than length of second end points");
+		else
+			System.out.println("Both lines are equal.");
 
 	}
 
